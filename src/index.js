d01366dc01f4f1ea0f445c416971c4cf6de1f23f
arindam dan <102957273+babudan@users.noreply.github.com>
2022-06-05 00:24:34 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Arindam-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
