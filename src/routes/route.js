9c01ee0ce9d0ceb89e121273a7989426d9b6e01d
arindam dan <102957273+babudan@users.noreply.github.com>
2022-06-04 13:47:28 +0530

    change code

--- src/routes/route.js
+++ src/routes/route.js
@@ -30,38 +30,6 @@ router.get('/candidates', function(req, res){
     let candidates = ['Arindam','Suman']
     res.send(candidates)
 })
-// router.get('/movies' , function(req, res){
-//           let movies = ["the dark night" , "intersteller" , "siccin" , "haunted"]
-//           res.send(movies)
-// })
-// router.get("/movies/:indexNumber" , function(req, res){
-//               let movies = ["the dark night" , "intersteller" , "siccin" , "haunted"]
-//                 res.send(movies[req.params.indexNumber])
-//           })
-          router.get("/movies/:indexNumber" , function(req, res){
-            let movies = ["the dark night" , "intersteller" , "siccin" , "haunted"]
-            if (req.params.indexNumber<movies.length){
-              res.send(movies[req.params.indexNumber])
-            }else {
-                res.send("tells the user to use a valid index")
-            }
-        })
-       router.get("/films" , function(req, res){
-      let cinema =   [ {
-            "id": 1,
-            "name": "The Shining"
-           }, {
-            "id": 2,
-            "name": "Incendies"
-           }, {
-            "id": 3,
-            "name": "Rang de Basanti"
-           }, {
-            "id": 4,
-            "name": "Finding Nemo"
-           }]
-        res.send(cinema)
-        })  
 
       
 
