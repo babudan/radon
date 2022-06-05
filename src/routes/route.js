d0374dc3f3620823340691ef7897d60fc60eca80
arindam dan <102957273+babudan@users.noreply.github.com>
2022-06-05 13:17:11 +0530

    param change

--- src/routes/route.js
+++ src/routes/route.js
@@ -15,7 +15,6 @@ router.get('/test-me', function (req, res) {
 });
 
 router.get('/hello', function (req, res) {
-   
     res.send('Hello there!')
 });
 
@@ -24,13 +23,20 @@ router.get('/candidates', function(req, res){
     let gender = req.query.gender
     let state = req.query.state
     let district = req.query.district
+    // let candidates = req.query.candidates
     console.log('State is '+state)
     console.log('Gender is '+gender)
     console.log('District is '+district)
+    // res.send(candidates)
     let candidates = ['Arindam','Suman']
     res.send(candidates)
+    // res.json({"name" : "arindam" , "age" : 23})
 })
-
+router.get('/candidates/:canidatesName', function(req, res){
+    // console.log('The request objects is '+ JSON.stringify(req.params))
+    console.log('Candidates name is '+req.params.canidatesName)
+    res.send('Done')
+})
       
 
 router.get('/candidates/:canidatesName', function(req, res){
